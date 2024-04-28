e86ce8228bab0e1026a41754d06273e19fa32c51
xxxg9991 <153571560+xxxg9991@users.noreply.github.com>
2024-04-28 18:48:22 +0900

    Add files via upload

--- kadai_017/src/kadai_017/KatoExce_Chapter17.java
+++ kadai_017/src/kadai_017/KatoExce_Chapter17.java
@@ -1,13 +1,10 @@
 package kadai_017;
+
 public class KatoExce_Chapter17 {
     public static void main(String[] args) {
-        KatoTaro_Chapter17  taro    = new KatoTaro_Chapter17();
+        KatoTaro_Chapter17 taro = new KatoTaro_Chapter17();
         KatoIchiro_Chapter17 ichiro = new KatoIchiro_Chapter17();
         KatoHanako_Chapter17 hanako = new KatoHanako_Chapter17();
-
-        //setGivenNameメソッドを呼び出す
-        taro.setGivenName();
-
 
         System.out.println("加藤太郎の紹介");
         taro.exceIntroduce();
--- kadai_017/src/kadai_017/KatoHanako_Chapter17.java
+++ kadai_017/src/kadai_017/KatoHanako_Chapter17.java
@@ -1,20 +1,18 @@
 package kadai_017;
 class KatoHanako_Chapter17 extends Kato_Chapter17 {
     public KatoHanako_Chapter17() {
-        super("加藤", "花子", "東京都中野区");
+        super("加藤", "花子", "東京都中野区〇×");
     }
 
     //引数なしのsetGivenNameメソッド
     @Override
     public void setGivenName() {
-        super.setGivenName("花子");
+        givenName = "花子";
     }
 
     //個別の紹介を出力するメソッドの実装
     @Override
     public void eachIntroduce() {
-        System.out.println("私の名前は" + familyName + givenName + "です");
-        System.out.println("住所は" + address + "です");
-        System.out.println("私はJavaが得意です");
+        System.out.println("趣味は読書です");
     }
 }
